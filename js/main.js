e383be536e3b781ce3bbb60802b252f2db7513c4
Wilder Ledes <wilder@Wilders-MacBook-Pro.local>
2022-08-09 17:03:57 -0400

    finished desktop version

--- js/main.js
+++ js/main.js
@@ -11,10 +11,10 @@ const swiper = new Swiper('.swiper', {
     },
 
     // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
 
     // Autoplay
     autoplay: {
